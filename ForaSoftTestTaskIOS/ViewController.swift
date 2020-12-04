f2eee94b8cf752ff4da0316cec23bd2c086e83d4
Artem <aporozov90@gmail.com>
2020-12-04 18:04:58 +0300

    Initial Commit

---
+++ ForaSoftTestTaskIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForaSoftTestTaskIOS
+//
+//  Created by Artem on 04.12.2020.
+//  Copyright © 2020 Artem P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
